fb128fb0ad5313e87ae2fab2a7290310ea7fa454
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2022-03-24 17:33:11 +0700

    Enable Experimental Command

--- src/commands/slash/TTS.ts
+++ src/commands/slash/TTS.ts
@@ -3,6 +3,7 @@ import { AutoBuilder, CocoaOption } from "cocoa-discord-utils/template";
 
 import { CommandInteraction } from "discord.js";
 
+import { AllGuilds } from "../shared";
 import { Voice } from "../shared/voice";
 
 export class TTS extends CogSlashClass {
@@ -13,16 +14,16 @@ export class TTS extends CogSlashClass {
     @SlashCommand(
         AutoBuilder("Speak!")
             .addStringOption(CocoaOption("text", "What to speak", true))
-            .addStringOption(CocoaOption("lang", "Language"))
+            .addStringOption(CocoaOption("lang", "Language")),
+        AllGuilds
     )
     async speak(ctx: CommandInteraction) {
         const text = ctx.options.getString("text", true);
         const lang = ctx.options.getString("lang");
 
+        await ctx.reply("わかります！");
+
         await Voice.joinFromContext(ctx);
-
         Voice.speak(ctx.guildId!, text, lang);
-
-        await ctx.reply("Done!");
     }
 }
